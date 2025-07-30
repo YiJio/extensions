719ffd2dbdb88706296447bd2ba9f6f9accd7680
unknown <jy.68171@nevada.unr.edu>
2025-07-30 01:34:22 -0700

    add: other projects (hidden), google recaptcha test

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-const fieldEmail = document.getElementById('email');
-const fieldCategory = document.getElementById('category');
-const fieldSubject = document.getElementById('subject');
-const fieldMessage = document.getElementById('message');
-
-function contact(e) {
+document.getElementById('form').addEventListener('submit', async(e) => {
 	e.preventDefault();
-}
+	const fieldEmail = document.getElementById('email').value;
+	const fieldCategory = document.getElementById('category').value;
+	const fieldSubject = document.getElementById('subject').value;
+	const fieldMessage = document.getElementById('message').value;
+	const captchaToken = grecaptcha.getResponse();
+	if(!captchaToken) {
+		alert('Please complete the CAPTCHA.');
+		return;
+	}
+	const res = await fetch('https://service.yijione.com/email/send', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ source: 'GitHub', email: fieldEmail, category: fieldCategory, subject: fieldSubject, message: fieldMessage, captchaToken }),
+	});
+	const data = await res.json();
+	if(data.error) {
+		alert('Failed: ' + data.error);
+	} else {
+		alert('Message sent!');
+		grecaptcha.reset();
+	}
+});
