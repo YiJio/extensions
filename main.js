32f8ca74f227289360f46ffa0c1aab63504907f9
unknown <jy.68171@nevada.unr.edu>
2025-07-30 03:05:26 -0700

    fix: contact form, captcha

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-window.addEventListener('load', () => {
-	alert('In order for CAPTCHA to work, please disable an ad blocker or try another browser without ad blockers.');
-  /*setTimeout(() => {
+/*window.addEventListener('load', () => {
+	alert('In order to send email successfully, the reCAPTCHA must work. However, for reCAPTCHA to work, please disable an ad blocker. If that still doesn\'t work, try another browser without any extensions installed.');
+  setTimeout(() => {
     if (typeof grecaptcha === 'undefined') {
       alert('CAPTCHA couldn\'t load. Please disable AdBlocker or try another browser.');
     }
-  }, 3000);*/
-});
+  }, 3000);
+});*/
 
 document.getElementById('form').addEventListener('submit', async(e) => {
 	e.preventDefault();
@@ -20,7 +20,7 @@ document.getElementById('form').addEventListener('submit', async(e) => {
 	}
 	const res = await fetch('https://service.yijione.com/email/send', {
 		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
+		headers: { 'Content-Type': 'application/json', 'X-Api-Key': 'SNm9B5SqF0RIFvnBdwedFGDzazUxQTqD' },
 		body: JSON.stringify({ source: 'GitHub', email: fieldEmail, category: fieldCategory, subject: fieldSubject, message: fieldMessage, captchaToken }),
 	});
 	const data = await res.json();
